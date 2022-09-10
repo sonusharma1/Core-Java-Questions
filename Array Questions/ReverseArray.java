c7a5575550f5f548f722a4bee88f1e01ddd62633
sonusharma1 <90684040+sonusharma1@users.noreply.github.com>
2022-09-10 17:43:45 +0530

    Update ReverseArray.java

--- Array Questions/ReverseArray.java
+++ Array Questions/ReverseArray.java
@@ -22,5 +22,21 @@ public class ReverseArray {
         for (int i : myArray) {
             System.out.print(i+" | ");
         }
+        
+        System.out.println();
+
+        int myArray2 [] = {1,2,3,4,5};
+        
+        // Method 2 swapping
+        int temp2 = 0, arraySize = myArray2.length;
+        for (int i = 0; i < myArray2.length/2; i++) {
+            temp2 = myArray2[i];
+            myArray2[i] = myArray2[arraySize-1];
+            myArray2[arraySize-1] = temp2;
+            arraySize--;
+        } 
+        for (int i : myArray2) {
+            System.out.print(i+" | ");
+        }  
     }
 }
